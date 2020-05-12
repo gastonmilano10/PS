99da6673a93da6f27ee6448be4ba2c68913e92cc
KevinPavon <65236059+KevinPavon@users.noreply.github.com>
2020-05-12 11:50:01 -0300

    sinc kevin
    pagina sincronizacion kevin

---
+++ src/pages/pag-sinc-1/pag-sinc-1.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PagSinc_1Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-pag-sinc-1',
+  templateUrl: 'pag-sinc-1.html',
+})
+export class PagSinc_1Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PagSinc_1Page');
+  }
+
+}
