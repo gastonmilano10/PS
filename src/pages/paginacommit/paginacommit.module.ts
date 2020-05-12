effcf79d93431308879130a9284b082838365816
Gaston Milano <gastonmilano15@hotmail.com>
2020-05-12 12:07:20 -0300

    Se creo pagina commit

---
+++ src/pages/paginacommit/paginacommit.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PaginacommitPage } from './paginacommit';
+
+@NgModule({
+  declarations: [
+    PaginacommitPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PaginacommitPage),
+  ],
+})
+export class PaginacommitPageModule {}
